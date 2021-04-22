96eaaff45c9e82285c126e851345e8788f070f48
windyStreet <yq904276384@foxmail.com>
2021-04-22 16:02:33 +0800

    add IOC

---
+++ Spring5/src/com/windystreet/spring/ioc/IOCUserDaoTest.java
@@ -0,0 +1,30 @@
+package com.windystreet.spring.ioc;
+
+import com.windystreet.spring.service.UserService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author windyStreet
+ * @codetime 2021-04-22 15:52
+ */
+public class IOCUserDaoTest {
+    /**
+     * 注入外部bean
+     */
+    @Test
+    public void test(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("UserDao.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        userService.add();
+    }
+
+    /**
+     * 注入内部bean
+     */
+    @Test
+    public void test1() {
+    }
+
+}
