b029e365c17702d16559d25c86a2df203ed86249
windyStreet <yq904276384@foxmail.com>
2021-04-23 10:54:19 +0800

    add IOC

---
+++ Spring5/src/com/windystreet/spring/ioc/lifecycle/Life.java
@@ -0,0 +1,26 @@
+package com.windystreet.spring.ioc.lifecycle;
+
+/**
+ * @author windyStreet
+ * @codetime 2021-04-23 10:29
+ */
+public class Life {
+    private String type;
+
+    public void setType(String type) {
+        System.out.println("调用set，执行了注入");
+        this.type = type;
+    }
+
+    public Life() {
+        System.out.println("执行了无参的构造函数");
+    }
+    // 创建执行的初始化的方法
+    public void initLife(){
+        System.out.println("执行初始化方法");
+    }
+
+    public void destroyLife(){
+        System.out.println("执行销毁方法");
+    }
+}
